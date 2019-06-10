ecac878a2f09057b0654cbcaa4b5942ee4ea3841
EEK <eek_official@icloud.com>
2019-06-10 19:51:07 +0300

    create user

--- jeleveux/settings.py
+++ jeleveux/settings.py
@@ -11,8 +11,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import django_heroku
-import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -122,6 +120,8 @@ STATIC_URL = '/static/'
 LOGIN_URL = '/login/'
 
 # Activate Django-Heroku.
+import django_heroku
+import dj_database_url
 django_heroku.settings(locals())
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
